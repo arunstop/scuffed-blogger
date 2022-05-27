b113d01867db4c7bf8fc7114e338d2ca8a1fd76d
arunstop <arunstop@gmail.com>
2022-05-28 02:44:15 +0700

    UPDATED UI on author page about section

--- components/main/MainLink.tsx
+++ components/main/MainLink.tsx
@@ -4,13 +4,14 @@ import React, { ReactNode } from "react";
 export interface MainLinkProps {
   href: string;
   children: ReactNode;
+  className?:string;
   newTab?: boolean;
 }
 
-function MainLink({ href, children, newTab }: MainLinkProps) {
+function MainLink({ href, children,className="hover:underline", newTab }: MainLinkProps) {
   return (
     <Link href={href} passHref>
-      <a className="hover:underline" target={newTab ? "_blank" : "_self"} rel="noopener noreferrer">{children}</a>
+      <a className={className} target={newTab ? "_blank" : "_self"} rel="noopener noreferrer">{children}</a>
     </Link>
   );
 }
--- components/user/UserAbout.tsx
+++ components/user/UserAbout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 import MainLink from "../main/MainLink";
 
-function linker(href: string, children: ReactNode, newTab?: boolean) {
+function linkify(href: string, children: ReactNode, newTab?: boolean) {
   return (
     <MainLink href={href} newTab={newTab}>
       {children}
@@ -9,9 +9,23 @@ function linker(href: string, children: ReactNode, newTab?: boolean) {
   );
 }
 
+interface UserAboutLink {
+  href: string;
+  title: string;
+}
+
+const links: UserAboutLink[] = [
+  { href: `https://www.instagram.com`, title: `See my Instagram profile` },
+  { href: `https://www.twitter.com`, title: `Hit me up on Twitter` },
+  { href: `https://www.facebook.com`, title: `Join my Facebook community` },
+  { href: `https://www.youtube.com`, title: `Subscribe my Youtube channel` },
+  { href: `https://www.github.com`, title: `Collaborate with me on Github` },
+  { href: `https://www.discord.com`, title: `Join the Discord server` },
+];
+
 export const UserAbout = ({ id }: { id: string }) => {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col gap-4 sm:gap-8">
       <p className="text-lg sm:text-xl">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore,
         eveniet quis enim tenetur impedit cumque molestias, qui ducimus
@@ -27,52 +41,36 @@ export const UserAbout = ({ id }: { id: string }) => {
         eveniet quis enim tenetur impedit cumque molestias, qui ducimus
         obcaecati dolores minima incidunt! Minus temporibus natus cum labore.
         Pariatur, modi similique.
-        <br />
-        <br />
-        <span>
-          Instagram :<br />{" "}
-          {linker(
-            `https://www.instagram.com`,
-            `https://www.instagram.com`,
-            true,
-          )}
-        </span>
-        <br />
-        <span>
-          Twitter :<br />{" "}
-          {linker(`https://www.twitter.com`, `https://www.twitter.com`, true)}
-        </span>
-        <br />
-        <span>
-          Facebook :<br />{" "}
-          {linker(`https://www.facebook.com`, `https://www.facebook.com`, true)}
-        </span>
-        <br />
-        <span>
-          Youtube :<br />{" "}
-          {linker(`https://www.youtube.com`, `https://www.youtube.com`, true)}
-        </span>
-        <br />
-        <span>
-          Github :<br />{" "}
-          {linker(`https://www.github.com`, `https://www.github.com`, true)}
-        </span>
-        <br />
       </p>
-      <br />
-      <p className="mt-4 gap-2 opacity-75 sm:mt-8 sm:gap-4">
+      <div className="flex flex-col gap-4 sm:gap-8">
+          <div className="text-lg sm:text-xl font-black">Connect :</div>
+          <div className="flex flex-wrap gap-2 sm:gap-4">
+            {links.map((e, idx) => (
+              <MainLink
+                key={idx}
+                href={e.href}
+                newTab
+                className="btn text-base sm:text-lg border-none h-auto py-2 px-4 sm:py-3 sm:px-6 decoration-2
+                btn-primary bg-opacity-10 text-primary hover:text-base-content gap-2 sm:gap-4 hover:underline"
+              >
+                {e.title}
+              </MainLink>
+            ))}
+          </div>
+        </div>
+      <p className="gap-2 opacity-75 sm:gap-4">
         <span>Joined since January 2022</span>
         <span className="mx-2 font-black sm:mx-4">&middot;</span>
         <span>
-          Member of {linker("/community/C++ Devs", "/C++ Devs")},{" "}
-          {linker(`/community/Quick`, `Quick Webdev`)},{" "}
-          {linker(`/community/Malang Photography`, `Malang Photography`)}
+          Member of {linkify("/community/C++ Devs", "C++ Devs")},{" "}
+          {linkify(`/community/Quick`, `Quick Webdev`)},{" "}
+          {linkify(`/community/Malang Photography`, `Malang Photography`)}
         </span>
         <span className="mx-2 font-black sm:mx-4">&middot;</span>
         <span>
-          Top writer in {linker(`/community/Programming`, `Programming`)},{" "}
-          {linker(`/community/Computer Science`, `Computer Science`)},{" "}
-          {linker(`/community/Photography`, `Photography`)}
+          Top writer in {linkify(`/topic/Programming`, `Programming`)},{" "}
+          {linkify(`/topic/Computer Science`, `Computer Science`)},{" "}
+          {linkify(`/topic/Photography`, `Photography`)}
         </span>
       </p>
     </div>
