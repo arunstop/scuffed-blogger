5d050064d94a7b6a85584cc589f5df5c9297a195
arunstop <arunstop@gmail.com>
2022-06-07 00:08:42 +0700

    ADDED progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UiProvider } from "../utils/contexts/ui/UiProvider";
 import { useRouteChange } from "../utils/hooks/RouteChangeHook";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
   // Detect route change
   useRouteChange();
   return (
     <UiProvider>
+      <NextNProgress
+        color="hsl(var(--pc))"
+        startPosition={0.3}
+        stopDelayMs={300}
+        height={6}
+        showOnShallow={false}
+      />
       <Component {...pageProps} />
     </UiProvider>
   );
