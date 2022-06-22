286a80dfcad374d3ddff4d89661ae207cc7f5008
arunstop <arunstop@gmail.com>
2022-06-22 22:17:17 +0700

    ADDED type guards on Falsy and ArticleModel

--- utils/data/Main.ts
+++ utils/data/Main.ts
@@ -2,3 +2,14 @@ export interface MainModalProps {
   value: boolean;
   onClose: () => void;
 }
+
+// CHECK FALSY
+// Falsy values :
+export type Falsy = false | 0 | -0 | 0n | "" | null | undefined;
+export function isFalsy(
+  value: unknown,
+): // check if value is Falsy with the `type deprecaation`
+value is Falsy {
+  // then compare `value` to false to see the result
+  return value == false;
+}
--- utils/data/models/Article.ts
+++ utils/data/models/Article.ts
@@ -1,3 +1,5 @@
+import { isFalsy } from "../Main";
+
 export interface ArticleModel {
   title: string;
   desc: string;
@@ -10,4 +12,25 @@ export interface ArticleModel {
   tags: string[];
   deleted: number;
   community?: string;
+}
+
+export function isArticleModel(value: unknown) {
+  // check if it is object
+  if (typeof value !== "object") return false;
+  // check falsy
+  if (isFalsy(value)) return false;
+  // check if there is a
+  if (
+    "title" in value &&
+    "desc" in value &&
+    "thumbnail" in value &&
+    "content" in value &&
+    "author" in value &&
+    "dateAdded" in value &&
+    "dateUpdated" in value &&
+    "duration" in value &&
+    "tags" in value &&
+    "deleted" in value
+  )
+    return true;
 }
