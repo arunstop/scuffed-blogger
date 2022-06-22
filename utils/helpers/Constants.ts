7d85d379e554a8c5db511cd541c6faab649df51a
arunstop <arunstop@gmail.com>
2022-06-23 00:33:55 +0700

    ADDED base_url based on environment

--- utils/helpers/Constants.ts
+++ utils/helpers/Constants.ts
@@ -2,6 +2,14 @@
 export const APP_NAME = "Tuturku";
 export const APP_DESC = "Scuffed blogs, for scuffed people";
 export const KEY_CLIENT_DARK_MODE = "DARK_MODE";
+export const KEY_ARTICLE_CONTENT = "DUMMY_ARTICLE_CONTENT";
+export const ON_PROD = process.env.NODE_ENV === "production";
+export const BASE_URL = ON_PROD
+  ? "https://tuturku.vercel.app"
+  : "http://localhost:3000";
+
+
+
 export const LOREM = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
 quo praesentium temporibus quod rerum labore officiis animi provident
 natus minus dolorem corrupti quos dolor unde vero vel, saepe tempora
@@ -9,7 +17,6 @@ obcaecati! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
 Voluptatibus quo praesentium temporibus quod rerum labore officiis
 animi provident natus minus dolorem corrupti quos dolor unde vero vel,
 saepe tempora obcaecati!`;
-export const KEY_ARTICLE_CONTENT = "DUMMY_ARTICLE_CONTENT";
 
 export const SEARCH_SUGGESTIONS_DUMMY = [
   { id: Math.floor(Math.random() * 100), title: "Lorem ipsum dolor sit amet" },
