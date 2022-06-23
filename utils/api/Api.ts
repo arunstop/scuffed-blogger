7d317ca24145f7003a3e55dc82946af36626b620
arunstop <arunstop@gmail.com>
2022-06-23 20:47:24 +0700

    ADDED callback to Api calls

--- utils/api/Api.ts
+++ utils/api/Api.ts
@@ -1,4 +1,6 @@
-import { ArticleModel } from './../data/models/Article';
+import { waitFor } from "../helpers/DelayHelpers";
+import { MainNetworkResponse } from "./../data/Main";
+import { ArticleModel } from "./../data/models/Article";
 import { axiosClient } from "./AxiosClient";
 // axios.get('/user?ID=12345')
 //   .then(function (response) {
@@ -32,22 +34,44 @@ export async function getArticleById() {
   return data;
 }
 
-export async function addArticle(newArticle: ArticleModel) {
+export async function addArticle({
+  article,
+  callback,
+}: {
+  article: ArticleModel;
+  callback?: (resp: MainNetworkResponse) => void;
+}) {
+  callback?.({
+    data: null,
+    message: "Submitting your article...",
+    status: "loading",
+  });
+  await waitFor(5000);
   let data: null | any = null;
   try {
     //   Call the endpoint
     const result = await axiosClient
-      .post("/api/article/add", JSON.stringify(newArticle), {
+      .post("/api/hello", JSON.stringify(article), {
         headers: {
           "Content-Type": "application/json; charset=utf-8",
         },
       })
       .then((resp) => {
         console.log(resp.data);
+        callback?.({
+          data: resp.data,
+          message: "Added to Database",
+          status: "success",
+        });
         return resp;
       });
     data = result.data;
   } catch (error) {
+    callback?.({
+      data: error + "",
+      message: "Error just occured",
+      status: "error",
+    });
     console.log(error);
   }
   return data;
