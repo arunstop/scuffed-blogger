8b72d260d5ab3bcda859e5239ad29369fdfe29a9
arunstop <arunstop@gmail.com>
2022-06-24 23:34:01 +0700

    ADDED loading placeholder back, with style similar to other placeholder

--- components/placeholder/LoadingPlaceholder.tsx
+++ components/placeholder/LoadingPlaceholder.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { MdWorkspaces } from "react-icons/md";
 interface LoadingPlaceholderProps {
-  text: string;
-  spinner?: boolean;
+  title: string;
+  desc: string;
+  actionLabel? :string;
+  action?: () => void;
 }
 function LoadingPlaceholder({
-  text,
-  spinner = false,
+  title,
+  desc,
+  actionLabel="Ok",
+  action,
 }: LoadingPlaceholderProps) {
   return (
     <div
-      className="wrap mx-auto my-4 flex items-center justify-center gap-2 
-      text-center text-lg font-bold sm:gap-4 sm:text-xl md:my-8 lg:my-12 xl:my-16"
+      className="flex flex-col mx-auto justify-center items-center 
+      bg-gradient-to-r from-transparent via-primary/40 dark:via-primary/20 to-transparent
+      gap-2 sm:gap-4 rounded-[10%] py-2 sm:py-4 px-4 sm:px-8 min-w-[50%] sm:max-w-2xl text-center
+      my-4 sm:my-8 pb-4"
     >
-      {spinner && (
-        <MdWorkspaces className="animate-spin text-2xl sm:text-3xl" />
-      )}
-      <span>{text}</span>
+      <MdWorkspaces className="text-[4rem] sm:text-[5rem] animate-spin text-primary" />
+      <p className="flex flex-col gap-1 sm:gap-2 text-center">
+        <span className="font-black text-2xl sm:text-3xl">{title}</span>
+        <span className="text-sm sm:text-base font-semibold">{desc}</span>
+      </p>
+      <button className=" btn --btn-resp" onClick={action}>
+        {actionLabel}
+      </button>
     </div>
   );
 }
