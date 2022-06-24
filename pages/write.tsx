ef9e549c810f16658f4d772289c1aab028da49cc
arunstop <arunstop@gmail.com>
2022-06-24 23:32:48 +0700

    Remove getserversideprops

--- pages/write.tsx
+++ pages/write.tsx
@@ -1,32 +1,31 @@
-import { GetServerSideProps } from "next";
 import Head from "next/head";
 import React from "react";
 import MainContainer from "../components/main/MainContainer";
 import WritingPanel from "../components/write/WritingPanel";
 import { APP_DESC, APP_NAME } from "../utils/helpers/Constants";
 
-export const getServerSideProps: GetServerSideProps<{ xd: any }> = async (
-  context,
-) => {
-  // SLUG ORDER
-  // 0 = User's id
-  // 1 = Tab/section
+// export const getServerSideProps: GetServerSideProps<{ xd: any }> = async (
+//   context,
+// ) => {
+//   // SLUG ORDER
+//   // 0 = User's id
+//   // 1 = Tab/section
 
-  const slug = context.query.authorslug || [];
-  const author = slug[0];
-  const tab = slug[1] || "post";
+//   const slug = context.query.authorslug || [];
+//   const author = slug[0];
+//   const tab = slug[1] || "post";
 
-  // const article = await Api.getArticleById();
-  const article = "";
-  // const article = await addArticle("HAHAHAHA");
-  // console.log(tab);
+//   // const article = await Api.getArticleById();
+//   const article = "";
+//   // const article = await addArticle("HAHAHAHA");
+//   // console.log(tab);
 
-  return {
-    props: { xd: article },
-  };
-};
-function Write({ xd }: { xd: any }) {
-  console.log(xd);
+//   return {
+//     props: { xd: article },
+//   };
+// };
+// function Write({ xd }: { xd: any }) {
+  function Write() {
   return (
     <>
       <Head>
