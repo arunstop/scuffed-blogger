9f5a0c2dbdd3cb2ad651ca1ff8f332db7dbc78e6
arunstop <arunstop@gmail.com>
2022-06-28 19:00:57 +0700

    ADDED forwarding ref

--- components/placeholder/LoadingIndicator.tsx
+++ components/placeholder/LoadingIndicator.tsx
@@ -4,12 +4,14 @@ interface LoadingIndicatorProps {
   text: string;
   spinner?: boolean;
 }
-function LoadingIndicator({
-  text,
-  spinner = false,
-}: LoadingIndicatorProps) {
+
+const LoadingIndicator = React.forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<LoadingIndicatorProps>
+>(({ text, spinner }, ref) => {
   return (
     <div
+    ref={ref}
       className="wrap mx-auto my-4 flex items-center justify-center gap-2 
       text-center text-lg font-bold sm:gap-4 sm:text-xl md:my-8 lg:my-12 xl:my-16"
     >
@@ -19,6 +21,22 @@ function LoadingIndicator({
       <span>{text}</span>
     </div>
   );
-}
+});
+
+LoadingIndicator.displayName = "LoadingIndicator";
+
+// function LoadingIndicator({ text, spinner = false }: LoadingIndicatorProps) {
+//   return (
+//     <div
+//       className="wrap mx-auto my-4 flex items-center justify-center gap-2
+//       text-center text-lg font-bold sm:gap-4 sm:text-xl md:my-8 lg:my-12 xl:my-16"
+//     >
+//       {spinner && (
+//         <MdWorkspaces className="animate-spin text-2xl sm:text-3xl" />
+//       )}
+//       <span>{text}</span>
+//     </div>
+//   );
+// }
 
 export default LoadingIndicator;
