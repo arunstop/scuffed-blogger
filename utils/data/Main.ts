0e1509adc53c15fa0d9c6187280ce923f56f8b9d
arunstop <arunstop@gmail.com>
2022-06-29 20:23:25 +0700

    ADDED factories

--- utils/data/Main.ts
+++ utils/data/Main.ts
@@ -3,14 +3,6 @@ export interface MainModalProps {
   onClose: () => void;
 }
 
-export type NetworkResponseStatus = "loading" | "success" | "error";
-
-export interface MainNetworkResponse<T=unknown>{
-  message: string;
-  status: NetworkResponseStatus;
-  data: T;
-}
-
 // CHECK FALSY
 // Falsy values :
 export type Falsy = false | 0 | -0 | 0n | "" | null | undefined;
@@ -21,3 +13,21 @@ value is Falsy {
   // then compare `value` to false to see the result
   return value == false;
 }
+
+export type NetworkResponseStatus = "loading" | "success" | "error";
+
+export interface MainNetworkResponse<T = unknown> {
+  message: string;
+  status: NetworkResponseStatus;
+  data: T;
+}
+
+// FACTORY
+export const createErrorResponse = (
+  message: string,
+): MainNetworkResponse<null> => ({ status: "error", data: null, message });
+
+export const createSuccessResponse = <T>(
+  message: string,
+  data: T,
+): MainNetworkResponse<T> => ({ status: "success", data: data, message });
