6cb3776402d506dd283d0f8c742b80424c236e61
arunstop <arunstop@gmail.com>
2022-06-30 04:30:50 +0700

    ADDED ui interaction for the planned Auth panel behaviour

--- components/auth/AuthPanel.tsx
+++ components/auth/AuthPanel.tsx
@@ -1,22 +1,56 @@
 import React, { useCallback, useState } from "react";
+import { MainNetworkResponse } from "../../utils/data/Main";
 import { APP_NAME } from "../../utils/helpers/Constants";
+import { useNetworkAction } from "../../utils/hooks/NetworkActionHook";
 import { scrollToTop } from "../../utils/hooks/RouteChangeHook";
 import GradientBackground from "../main/GradientBackground";
+import StatusPlaceholder, {
+  StatusPlaceholderProps,
+} from "../placeholder/StatusPlaceholder";
 import AuthLoginForm from "./AuthLoginForm";
 import AuthRegisterForm from "./AuthRegisterForm";
 import AuthResetPwForm from "./AuthResetPwForm";
 import AuthSocialSection from "./AuthSocialSection";
 
 export type AuthFormTypes = "LOGIN" | "REGISTER" | "RESET_PW";
+interface SetStatusPropsProps {
+  newLoading: boolean;
+  newPlaceHolder: StatusPlaceholderProps;
+  newNetResp?: MainNetworkResponse<string | null>;
+}
 export interface AuthFormProps {
   changeForm: (form: AuthFormTypes) => void;
+  setAction: ({
+    newLoading,
+    newPlaceHolder,
+    newNetResp,
+  }: SetStatusPropsProps) => void;
+  cancelActions: () => void;
 }
 function AuthPanel() {
   const [form, setForm] = useState<AuthFormTypes>("LOGIN");
+  const [placeholder, setPlaceholder] = useState<StatusPlaceholderProps>();
+  const { loading, setLoading, netResp, setNetResp } = useNetworkAction<
+    string | null
+  >();
 
-  const changeForm = useCallback((form: AuthFormTypes) => {
+  const changeForm = useCallback((selectedForm: AuthFormTypes) => {
     scrollToTop(true);
-    setForm(form);
+    setForm(selectedForm);
+  }, []);
+
+  const setAction = useCallback(
+    ({ newLoading, newPlaceHolder, newNetResp }: SetStatusPropsProps) => {
+      setLoading(newLoading);
+      setPlaceholder(newPlaceHolder);
+      if (newNetResp) setNetResp(newNetResp);
+    },
+    [placeholder],
+  );
+
+  const cancelActions = useCallback(() => {
+    setLoading(false);
+    setPlaceholder(undefined);
   }, []);
 
   const renderTitle = () => {
@@ -46,32 +80,47 @@ function AuthPanel() {
         break;
     }
   };
-
-  const renderCurrentForm = () => {
-    switch (form) {
-      case "LOGIN":
-        return <AuthLoginForm changeForm={changeForm} />;
-      case "REGISTER":
-        return <AuthRegisterForm changeForm={changeForm} />;
-      case "RESET_PW":
-        return <AuthResetPwForm changeForm={changeForm} />;
-      default:
-        break;
-    }
-  };
-
   return (
-    <div className="relative z-0 overflow-hidden rounded-xl">
-      <GradientBackground />
-
+    <div className="relative z-0 overflow-hidden rounded-xl min-h-screen">
+      {!loading && !placeholder && <GradientBackground />}
       <div className="mx-auto flex w-full flex-col gap-4 p-4 sm:max-w-md sm:gap-8 sm:p-8 md:max-w-lg lg:max-w-xl">
         <span className="my-1 text-center text-2xl font-black text-primary-content sm:my-3 sm:text-3xl">
           {renderTitle()}
         </span>
-        <div className="form-control  gap-4 sm:gap-8">
-          {renderCurrentForm()}
-          <AuthSocialSection />
-        </div>
+
+        {loading && placeholder && <StatusPlaceholder {...placeholder} />}
+        {!loading && netResp?.status === "error" && placeholder && (
+          <StatusPlaceholder {...placeholder} />
+        )}
+        {!loading && netResp?.status === "success" && placeholder && (
+          <StatusPlaceholder {...placeholder} />
+        )}
+        {!loading && !placeholder && (
+          <div className="form-control  gap-4 sm:gap-8">
+            {form === "LOGIN" && (
+              <AuthLoginForm
+                setAction={setAction}
+                changeForm={changeForm}
+                cancelActions={cancelActions}
+              />
+            )}
+            {form === "REGISTER" && (
+              <AuthRegisterForm
+                setAction={setAction}
+                changeForm={changeForm}
+                cancelActions={cancelActions}
+              />
+            )}
+            {form === "RESET_PW" && (
+              <AuthResetPwForm
+                setAction={setAction}
+                changeForm={changeForm}
+                cancelActions={cancelActions}
+              />
+            )}
+            <AuthSocialSection />
+          </div>
+        )}
       </div>
     </div>
   );
--- components/auth/AuthRegisterForm.tsx
+++ components/auth/AuthRegisterForm.tsx
@@ -2,16 +2,136 @@ import React from "react";
 import { MdEmail, MdLock } from "react-icons/md";
 import MainTextInput from "../input/MainTextInput";
 import { AuthFormProps } from "./AuthPanel";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { waitFor } from "../../utils/helpers/DelayHelpers";
+import { createErrorResponse } from "../../utils/data/Main";
 
-function AuthRegisterForm({ changeForm }: AuthFormProps) {
+interface InputFields {
+  email: string;
+  password: string;
+}
+
+function AuthRegisterForm({
+  changeForm,
+  setAction,
+  cancelActions,
+}: AuthFormProps) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<InputFields>({ mode: "onChange" });
+
+  function actionLoading() {
+    setAction({
+      newLoading: true,
+      newPlaceHolder: {
+        title: "Processing your registration...",
+        desc: `Hold up for a moment, we are registering your information into our database, 
+          checking things left right and center as usual, you know the drill`,
+        status: "loading",
+        actions: [
+          {
+            callback: cancelActions,
+            label: "Cancel",
+          },
+        ],
+      },
+    });
+  }
+
+  function actionError(data: InputFields) {
+    setAction({
+      newLoading: false,
+      newNetResp: createErrorResponse("Well something happened..."),
+      newPlaceHolder: {
+        title: "Something weird happened...",
+        desc: `Sorry that this happened. But an error has occured, stating:\nError, something something something`,
+        status: "error",
+        actions: [
+          {
+            callback: cancelActions,
+            label: "Cancel",
+          },
+          {
+            callback: () => onSubmit(data),
+            label: "Try again",
+          },
+          {
+            callback: actionSuccess,
+            label: "Not really",
+          },
+        ],
+      },
+    });
+  }
+
+  function actionSuccess() {
+    setAction({
+      newLoading: true,
+      newPlaceHolder: {
+        title: "Registration completed!",
+        desc: `Welcome to Tuturku! When thoughts meet each other. Well that was smooth, wasn't it? 
+        Explore our articles or you can write your own.`,
+        status: "success",
+        actions: [
+          {
+            callback: cancelActions,
+            label: "Explore",
+          },
+          {
+            callback: cancelActions,
+            label: "Write my first article",
+          },
+        ],
+      },
+    });
+  }
+  // console.log(watch("email"));
+
+  const onSubmit: SubmitHandler<InputFields> = async (data) => {
+    actionLoading();
+    await waitFor(3000);
+    actionError(data);
+  };
+  // const confirm = (ev: React.FormEvent<HTMLFormElement>) => {
+  //   ev.preventDefault();
+  //   alert("sd");
+  // };
+
   return (
-    <>
-      <MainTextInput type="email" placeholder="Email..." icon={<MdEmail />} />
+    <form
+      className="form-control  gap-4 sm:gap-8"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <MainTextInput
+        type="email"
+        placeholder="Email..."
+        icon={<MdEmail />}
+        {...register("email", {
+          required: { value: true, message: "Email cannot be empty" },
+          pattern: {
+            value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+            message: "Email requires a valid format",
+          },
+        })}
+        error={!!errors.email}
+        errorMsg={errors.email?.message}
+      />
       <MainTextInput
         type="password"
         placeholder="Password..."
         icon={<MdLock />}
-        minLength={8}
+        {...register("password", {
+          required: { value: true, message: "Password cannot be empty" },
+          minLength: {
+            value: 8,
+            message: "Password should be 8 characters minimum",
+          },
+        })}
+        error={!!errors.password}
+        errorMsg={errors.password?.message}
       />
 
       <button className="btn --btn-resp btn-primary text-lg font-bold sm:text-xl">
@@ -22,13 +142,14 @@ function AuthRegisterForm({ changeForm }: AuthFormProps) {
       </span>
 
       <button
+        type="submit"
         className="btn --btn-resp btn-primary flex-1 flex-nowrap gap-x-2
             text-clip text-lg font-bold sm:gap-x-4 sm:text-xl"
         onClick={() => changeForm("LOGIN")}
       >
         Login Instead
       </button>
-    </>
+    </form>
   );
 }
 
