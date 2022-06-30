6cf1042937f0d0d6947f7520ec7b15888b573852
arunstop <arunstop@gmail.com>
2022-07-01 01:20:08 +0700

    ADDED more transition helpers

--- utils/helpers/UiTransitionHelpers.ts
+++ utils/helpers/UiTransitionHelpers.ts
@@ -15,11 +15,24 @@ export const transitionPullV = (
 export const transitionFade = (
   props?: UiTransitionProps,
 ): UiTransitionProps => ({
-  enter: `ease-out transition duration-500 ${props?.enter || ""}`,
+  enter: `ease-out transition duration-300 ${props?.enter || ""}`,
   enterFrom: `opacity-0 ${props?.enterFrom || ""}`,
   enterTo: `opacity-100 ${props?.enterTo || ""}`,
   entered: `${props?.entered}`,
-  leave: `ease-in transition duration-300 ${props?.leave || ""}`,
-  leaveFrom: `opacity-100 ${props?.leaveFrom || ""}`,
-  leaveTo: `opacity-0 ${props?.leaveTo || ""}`,
+  // leave: `ease-in transition duration-300 ${props?.leave || ""}`,
+  // leaveFrom: `opacity-100 ${props?.leaveFrom || ""}`,
+  // leaveTo: `opacity-0 ${props?.leaveTo || ""}`,
+});
+
+
+export const transitionScaleY = (
+  props?: UiTransitionProps,
+): UiTransitionProps => ({
+  enter: `ease-out transform transition duration-300 ${props?.enter || ""}`,
+  enterFrom: `opacity-0 scale-y-0 ${props?.enterFrom || ""}`,
+  enterTo: `opacity-100 scale-y-100 ${props?.enterTo || ""}`,
+  entered: `${props?.entered}`,
+  // leave: `ease-in transform transition duration-300 ${props?.leave || ""}`,
+  // leaveFrom: `opacity-100 scale-y-100 ${props?.leaveFrom || ""}`,
+  // leaveTo: `opacity-0 scale-y-0 ${props?.leaveTo || ""}`,
 });
