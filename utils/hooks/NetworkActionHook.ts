3e27830f92983d9534ea6be0531b2ffd194e92b8
arunstop <arunstop@gmail.com>
2022-07-01 01:20:46 +0700

    IMPROVED functionality by adding generic typing

--- utils/hooks/NetworkActionHook.ts
+++ utils/hooks/NetworkActionHook.ts
@@ -1,9 +1,16 @@
-import { MainNetworkResponse } from "./../data/Main";
 import { useState } from "react";
+import { MainNetworkResponse } from "./../data/Main";
 
-export function useNetworkAction<T>() {
-  const [loading, setLoading] = useState(false);
-  const [netResp, setNetResp] = useState<MainNetworkResponse<T>>();
+// deciding the type wether on the generic type is empty or not
+type LoadingProps<T> = T extends undefined
+  ? boolean
+  : { value: boolean; data?: T };
+
+export function useNetworkAction<T1, T2 = undefined>(initLoading:LoadingProps<T2>) {
+  // loading state
+  const [loading, setLoading] = useState<LoadingProps<T2>>(initLoading);
+  // loaded states
+  const [netResp, setNetResp] = useState<MainNetworkResponse<T1>>();
 
   return { loading, setLoading, netResp, setNetResp };
 }
