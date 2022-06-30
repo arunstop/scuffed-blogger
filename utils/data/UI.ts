0cc99cb97df058d7f0c8c92b16ed03cb3f2a1339
arunstop <arunstop@gmail.com>
2022-06-30 23:52:58 +0700

    ADDED transition helper

--- utils/data/UI.ts
+++ utils/data/UI.ts
@@ -1,1 +1,10 @@
 export type BreakpointTypes = "sm" | "md" | "lg" | "xl";
+export interface UiTransitionProps {
+  enter?: string;
+  enterFrom?: string;
+  enterTo?: string;
+  entered?:string;
+  leave?: string;
+  leaveFrom?: string;
+  leaveTo?: string;
+}
---
+++ utils/helpers/UiTransitionHelpers.ts
@@ -0,0 +1,25 @@
+import { UiTransitionProps } from "./../data/UI";
+
+export const transitionPullV = (
+  props?: UiTransitionProps,
+): UiTransitionProps => ({
+  enter: `ease-out transform transition duration-500 ${props?.enter || ""}`,
+  enterFrom: `opacity-0 translate-y-[20%] scale-x-0 ${props?.enterFrom || ""}`,
+  enterTo: `opacity-100 translate-y-0 scale-x-100 ${props?.enterTo || ""}`,
+  entered: `${props?.entered}`,
+  leave: `ease-in transform transition duration-300 ${props?.leave || ""}`,
+  leaveFrom: `opacity-100 translate-y-0 scale-x-100 ${props?.leaveFrom || ""}`,
+  leaveTo: `opacity-0 translate-y-[20%] scale-x-0 ${props?.leaveTo || ""}`,
+});
+
+export const transitionFade = (
+  props?: UiTransitionProps,
+): UiTransitionProps => ({
+  enter: `ease-out transition duration-500 ${props?.enter || ""}`,
+  enterFrom: `opacity-0 ${props?.enterFrom || ""}`,
+  enterTo: `opacity-100 ${props?.enterTo || ""}`,
+  entered: `${props?.entered}`,
+  leave: `ease-in transition duration-300 ${props?.leave || ""}`,
+  leaveFrom: `opacity-100 ${props?.leaveFrom || ""}`,
+  leaveTo: `opacity-0 ${props?.leaveTo || ""}`,
+});
