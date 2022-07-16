884c2ffe99a6dff7e4899b47e9dc57cdbb882331
arunstop <arunstop@gmail.com>
2022-07-17 02:28:37 +0700

    add : label to text area

--- components/input/MainTextAreaInput.tsx
+++ components/input/MainTextAreaInput.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 
-type MainTextAreaInputProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
-// & {
-//   clearable?: boolean;
-//   clearIcon?: boolean;
-//   clearAction?: () => void;
-//   icon?: ReactNode;
-// }
+type MainTextAreaInputProps =
+  React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+    // clearable?: boolean;
+    // clearIcon?: boolean;
+    // clearAction?: () => void;
+    // icon?: ReactNode;
+    label?: string;
+  };
 
 const MainTextAreaInput = React.forwardRef<
   HTMLTextAreaElement,
   MainTextAreaInputProps
->(({ ...props }, ref) => {
+>(({ label, ...props }, ref) => {
   return (
-    <label
-      className={`input-group-sm input-group relative rounded-xl sm:input-group-md`}
-    >
-      <textarea
-        ref={ref}
-        {...props}
-        className={`peer textarea w-full !rounded-xl text-sm md:text-base
+    <div className="flex w-full flex-col gap-1 sm:gap-2">
+      {label && <span className={`text-md sm:text-lg`}>{label}</span>}
+      <label
+        className={`input-group-sm input-group relative rounded-xl sm:input-group-md`}
+      >
+        <textarea
+          ref={ref}
+          {...props}
+          className={`peer textarea w-full !rounded-xl text-sm md:text-base
         !outline !outline-base-content/100 !outline-1 !outline-offset-0
         focus:!outline-[2px] sm:focus:!outline-[3px] focus:z-[2] focus:border-transparent 
         focus:valid:!outline-base-content duration-300
@@ -30,9 +33,10 @@ const MainTextAreaInput = React.forwardRef<
         px-2 md:px-4 py-1 md:py-2
         ${props.className}
         `}
-        placeholder={props.placeholder || ". . . ."}
-      />
-    </label>
+          placeholder={props.placeholder || ". . . ."}
+        />
+      </label>
+    </div>
   );
 });
 
