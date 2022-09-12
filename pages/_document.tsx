37c07126ff1ad63137f834c2438b885bce90e78c
scuffed-developer <scuffed.developer@gmail.com>
2022-09-13 02:39:28 +0700

    fix : title not showing when lazy loading the whole app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { APP_NAME, APP_DESC } from "../utils/helpers/Constants";
 
 export default function Document() {
   return (
     <Html lang="en" className="!overflow-auto !pr-0">
-      <Head />
+      <Head>
+        <title>{`${APP_NAME} - Let your voice be heard`}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={APP_DESC} />
+      </Head>
       <body className="max-h-screen overflow-auto overflow-x-hidden font-nunito transition-colors duration-500">
         <Main />
         <NextScript />
