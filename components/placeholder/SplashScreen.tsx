44ef84e0d6f19cd7ddc9c461a45512fa8aacd087
arunstop <arunstop@gmail.com>
2022-09-15 22:56:14 +0700

    update : redesign the splash screen

--- components/placeholder/SplashScreen.tsx
+++ components/placeholder/SplashScreen.tsx
@@ -3,15 +3,21 @@ import { APP_NAME } from "../../utils/helpers/Constants";
 
 function SplashScreen() {
   return (
-    <div
-      id="splash-screen"
-      className="flex min-h-screen animate-twPulse  bg-gradient-to-b from-transparent via-primary/50  to-transparent
-      transition-all"
-    >
-      <div className="sm-gap-4 m-auto flex animate-bounce flex-col items-center gap-2 text-primary-content ">
-        <MdWorkspaces className="animate-spin text-4xl sm:text-5xl" />
+    <div id="splash-screen" className="flex min-h-screen relative">
+      {/* background */}
+      <div
+        className="animate-twPulse animate-infinite  bg-gradient-to-b from-transparent via-primary/50  to-transparent
+      transition-all absolute inset-0 "
+      ></div>
+      {/* spinning and label */}
+      <div className="m-auto flex flex-col items-center">
+        <div className="sm-gap-4 m-auto flex animate-twBounce animate-infinite flex-col items-center gap-2 text-primary-content ">
+          <div className="animate-twSpin animate-infinite ">
+            <MdWorkspaces className="animate-flip animate-infinite text-4xl sm:text-5xl" />
+          </div>
 
-        <span className="text-xl font-black sm:text-2xl">{APP_NAME}</span>
+          <span className="text-xl font-black sm:text-2xl">{APP_NAME}</span>
+        </div>
       </div>
     </div>
   );
