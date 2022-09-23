cca13a4d776aeef4fa468960493ab86f3bfa8d9d
arunstop <arunstop@gmail.com>
2022-09-23 17:56:00 +0700

    update : added actions (optional) on mobile header

--- components/main/MobileHeader.tsx
+++ components/main/MobileHeader.tsx
@@ -1,28 +1,46 @@
+import { ReactNode } from "react";
 import { MdArrowBack } from "react-icons/md";
 
-function MobileHeader({
-  title,
-  back,
-}: {
+interface MobileHeaderProps {
   title: string;
-  back: () => void;
-}) {
+  back?: () => void;
+  actions?: { label: string; icon: ReactNode; action: () => void }[];
+}
+function MobileHeader({ title, back, actions }: MobileHeaderProps) {
   return (
     <div
-      className="sm:hidden sticky inset-x-0 top-0 flex gap-2 items-center z-[1] bg-base-300/90 p-2 
-       border-b-2 border-primary-content/10 -mt-2"
+      className="sm:hidden sticky inset-x-0 top-0 flex gap-2 justify-between items-center z-[1] bg-base-100/70 p-2 
+       -mt-2 backdrop-blur-md"
     >
-      {back && (
-        <button
-          className="btn btn-circle btn-ghost self-start min-h-0 !h-9 !w-9"
-          onClick={() => {
-            back?.();
-          }}
-        >
-          <MdArrowBack className="text-3xl text-primary" />
-        </button>
+      <div className="flex gap-2">
+        {back && (
+          <button
+            className="btn btn-circle btn-ghost self-start min-h-0 !h-9 !w-9"
+            onClick={() => {
+              back?.();
+            }}
+          >
+            <MdArrowBack className="text-3xl text-primary" />
+          </button>
+        )}
+        <span className="text-2xl font-bold">{title}</span>
+      </div>
+      {!!actions?.length && (
+        <div className="flex gap-2">
+          {actions.map((e, idx) => {
+            return (
+              <button
+                key={idx}
+                className="btn rounded-xl btn-sm btn-ghost text-xl text-primary"
+                onClick={e.action}
+                title={e.label}
+              >
+                {e.icon}
+              </button>
+            );
+          })}
+        </div>
       )}
-      <span className="text-2xl font-bold">{title}</span>
     </div>
   );
 }
