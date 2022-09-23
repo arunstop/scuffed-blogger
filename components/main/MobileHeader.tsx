61f81a8302cafbfa1ba1122697c334bea38d11a9
arunstop <arunstop@gmail.com>
2022-09-23 18:12:21 +0700

    update : added enter animation on mobile header

--- components/main/MobileHeader.tsx
+++ components/main/MobileHeader.tsx
@@ -10,7 +10,7 @@ function MobileHeader({ title, back, actions }: MobileHeaderProps) {
   return (
     <div
       className="sm:hidden sticky inset-x-0 top-0 flex gap-2 justify-between items-center z-[1] bg-base-100/70 p-2 
-       -mt-2 backdrop-blur-md"
+       -mt-2 backdrop-blur-md animate-fadeInDown"
     >
       <div className="flex gap-2">
         {back && (
@@ -31,7 +31,7 @@ function MobileHeader({ title, back, actions }: MobileHeaderProps) {
             return (
               <button
                 key={idx}
-                className="btn rounded-xl btn-sm btn-ghost text-xl text-primary"
+                className="btn rounded-xl btn-sm btn-ghost text-2xl text-primary"
                 onClick={e.action}
                 title={e.label}
               >
