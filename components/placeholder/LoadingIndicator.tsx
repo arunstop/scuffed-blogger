7807d0c57a623beee10ffa1c1437e3fdeed3d75a
arunstop <arunstop@gmail.com>
2022-09-24 19:14:18 +0700

    update : made title props on laoding indicator optional

--- components/placeholder/LoadingIndicator.tsx
+++ components/placeholder/LoadingIndicator.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MdWorkspaces } from "react-icons/md";
 interface LoadingIndicatorProps {
-  text: string;
+  text?: string;
   spinner?: boolean;
 }
 
@@ -11,14 +11,14 @@ const LoadingIndicator = React.forwardRef<
 >(({ text, spinner }, ref) => {
   return (
     <div
-    ref={ref}
+      ref={ref}
       className="wrap mx-auto my-4 flex items-center justify-center gap-2 
       text-center text-lg font-bold sm:gap-4 sm:text-xl md:my-8 lg:my-12 xl:my-16"
     >
       {spinner && (
         <MdWorkspaces className="animate-twSpin animate-infinite text-2xl sm:text-3xl " />
       )}
-      <span>{text}</span>
+      {text && <span>{text}</span>}
     </div>
   );
 });
