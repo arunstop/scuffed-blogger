70d49cbf1c46c731f7871d42c8fa65a9d808370e
arunstop <arunstop@gmail.com>
2022-09-27 12:58:09 +0700

    fix : applying more effective and efficient logic to modalrouted hook for opening and closing modal

--- utils/hooks/ModalRoutedBehaviorHook.ts
+++ utils/hooks/ModalRoutedBehaviorHook.ts
@@ -1,33 +1,47 @@
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
-import { routeTrimQuery } from "../helpers/MainHelpers";
 
-export function useModalRoutedBehaviorHook(
-  mainParam: string,
-) {
+export function useModalRoutedBehaviorHook(mainParam: string) {
   const [show, setShow] = useState(false);
   const router = useRouter();
 
+  const value = router.query[mainParam];
+
   // toggle modal
-  const toggle = useCallback((value: boolean) => {
-    setShow(value);
-  }, []);
+  const toggle = useCallback(
+    (show: boolean, value?: any) => {
+      // if showing
+      if (show)
+        return router.push(
+          {
+            query: {
+              ...router.query,
+              [mainParam]: value,
+            },
+          },
+          undefined,
+          { shallow: true },
+        );
+      // if hiding
+      return close();
+    },
+    [router],
+  );
 
   // close state
   const close = useCallback(() => {
-    const currentPath = routeTrimQuery(router.asPath);
-    router.replace(currentPath, undefined, {
-      shallow: true,
-    });
-  }, [router]);
-
-  const value = router.query[mainParam];
+    // const currentPath = routeTrimQuery(router.asPath);
+    // router.replace(currentPath, undefined, {
+    //   shallow: true,
+    // });
+    if(value) router.back();
+  }, [value]);
 
   // Determine on what query/param on url, the state will change
   useEffect(() => {
     // if query "postoption" exist, it will open the modal
     // if it doesn't it will close the modal
-    toggle(!!value);
+    setShow(!!value);
 
     return () => {};
   }, [value]);
