42445abb3de08972ccb6bf6df94798eb510311e7
arunstop <arunstop@gmail.com>
2022-09-29 00:52:53 +0700

    update : added optional props `options` to MobileHeader to show dropdown

--- components/main/MobileHeader.tsx
+++ components/main/MobileHeader.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import { MdArrowBack } from "react-icons/md";
 import { scrollToTop } from "../../utils/hooks/RouteChangeHook";
+import Dropdown, { DropdownOption } from "./Dropdown";
 
 export interface MobileHeaderActionProps {
   label: string;
   icon: ReactNode;
-  action: () => void;
+  action?: () => void;
+  options?: DropdownOption[];
   disabled?: boolean;
 }
 
@@ -19,13 +21,13 @@ interface MobileHeaderProps {
 function MobileHeader({ title, back, actions, toTop }: MobileHeaderProps) {
   return (
     <div
-      className="sm:hidden sticky inset-x-0 top-0 flex gap-2 justify-between items-center z-[10] bg-base-100/70 p-2 
-       -mt-2 backdrop-blur-md animate-fadeInDown animate-duration-500"
+      className="sticky inset-x-0 top-0 z-[10] -mt-2 flex animate-fadeInDown items-center justify-between gap-2 bg-base-100/70 
+       p-2 backdrop-blur-md animate-duration-500 sm:hidden"
     >
-      <div className="flex gap-1 items-center">
+      <div className="flex items-center gap-1">
         {back && (
           <button
-            className="btn rounded-xl btn-sm btn-ghost text-2xl text-primary aspect-square p-0 self-start"
+            className="btn-ghost btn-sm btn aspect-square self-start rounded-xl p-0 text-2xl text-primary"
             onClick={() => {
               back?.();
             }}
@@ -35,25 +37,27 @@ function MobileHeader({ title, back, actions, toTop }: MobileHeaderProps) {
         )}
         <span
           className={`text-xl font-bold cursor-pointer`}
-          onClick={() => toTop ? toTop():scrollToTop(true)}
+          onClick={() => (toTop ? toTop() : scrollToTop(true))}
         >
           {title}
         </span>
       </div>
       {!!actions?.length && (
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           {actions.map((e, idx) => {
             return (
-              <button
-                key={idx}
-                className={`btn rounded-xl btn-sm  text-2xl  aspect-square p-0
-                ${e.disabled ? "!bg-opacity-10" : "btn-ghost text-primary"}`}
-                onClick={e.disabled ? undefined : e.action}
-                title={e.label}
-                disabled={e.disabled}
-              >
-                {e.icon}
-              </button>
+              <Dropdown key={idx} options={e.options || []} className="dropdown-end">
+                <button
+                  tabIndex={0}
+                  className={`btn rounded-xl btn-sm  text-2xl  aspect-square p-0
+                  ${e.disabled ? "!bg-opacity-10" : "btn-ghost text-primary"}`}
+                  onClick={e.disabled ? undefined : e.action}
+                  title={e.label}
+                  disabled={e.disabled}
+                >
+                  {e.icon}
+                </button>
+              </Dropdown>
             );
           })}
         </div>
