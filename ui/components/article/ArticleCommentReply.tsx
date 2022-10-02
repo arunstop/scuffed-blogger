f1109d76dc96fa79cb3110bcc3148f97e964335d
arunstop <arunstop@gmail.com>
2022-10-03 03:07:02 +0700

    update : added toggle button to show replies

--- ui/components/article/ArticleCommentReply.tsx
+++ ui/components/article/ArticleCommentReply.tsx
@@ -11,39 +11,52 @@ function ArticleCommentReply({ comment }: { comment: CommentModel }) {
   const [show, setShow] = useState(false);
   const count = comment.replies?.length || 0;
 
-  const loadReplies = useCallback(async () => {
-    // if this comment is somehow a reply, don't do anything
-    if (comment.parentCommentId) return;
-    const repliesFromDb = await serviceCommentReplyGetByParent({
-      data: {
-        articleId: comment.articleId,
-        parentCommentId: comment.id,
-        count: 5,
-        start: replies?.offset || 0,
-      },
-    });
-    if (!repliesFromDb) return console.log("no replies for some reason");
-    setReplies(repliesFromDb);
-    setShow(true);
-    console.log(repliesFromDb);
-  }, [replies]);
+  const loadReplies = useCallback(
+    async (startFrom?: number) => {
+      // if this comment is somehow a reply, don't do anything
+      if (comment.parentCommentId) return;
+      const repliesFromDb = await serviceCommentReplyGetByParent({
+        data: {
+          articleId: comment.articleId,
+          parentCommentId: comment.id,
+          count: 5,
+          start: startFrom ?? (replies?.offset || 0),
+        },
+      });
+      if (!repliesFromDb) return console.log("no replies for some reason");
+      setReplies(repliesFromDb);
+      setShow(true);
+      console.log(repliesFromDb);
+    },
+    [replies],
+  );
+
   const isAllShown = replies?.total || 0 <= (replies?.comments.length || 0);
+
+  const toggleReplies = useCallback(() => {
+    if (show) return setShow(false);
+    return loadReplies(0);
+  }, [show]);
+
   return (
     <>
+      <button
+        className="btn btn-outline rounded-xl btn-xs sm:btn-sm mr-auto"
+        onClick={toggleReplies}
+      >
+        {show
+          ? `${count == 1 ? "Hide reply" : `Hide replies`}`
+          : `${count == 1 ? "Show reply" : `Show ${count} replies`}`}
+      </button>
       {show && !!replies && (
-        <div className={`flex flex-col ${!isAllShown ? "mb-4 sm:mb-8" : ""} border-2 border-base-content/10 rounded-xl`}>
+        <div
+          className={`flex flex-col ${
+            !isAllShown ? "mb-4 sm:mb-8" : ""
+          } mt-2 sm:mt-4 border-2 border-base-content/10 rounded-xl`}
+        >
           <ArticleComments commentList={replies} lined />
         </div>
       )}
-      {!isAllShown ||
-        (!replies && (
-          <button
-            className="btn btn-outline rounded-xl btn-xs sm:btn-sm mr-auto"
-            onClick={() => loadReplies()}
-          >
-            {count == 1 ? "Show reply" : `Show ${count} replies`}
-          </button>
-        ))}
     </>
   );
 }
