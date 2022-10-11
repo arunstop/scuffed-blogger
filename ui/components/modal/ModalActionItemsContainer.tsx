5ae17da7ff305dc7999b978a6573e812c043c02e
arunstop <arunstop@gmail.com>
2022-10-11 09:53:11 +0700

    fix : modal action items now won't diappear when closing the modal

---
+++ ui/components/modal/ModalActionItemsContainer.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ModalActionItem from "./ModalActionItem";
+import {
+  ModalActionAction,
+  ModalActionConfirmation,
+} from "./ModalActionTemplate";
+
+function ModalActionItemsContainer({
+  actions,
+  openConfirmation,
+}: {
+  actions: ModalActionAction[];
+  openConfirmation: (value: ModalActionConfirmation | undefined) => void;
+}) {
+  return (
+    <>
+      {actions.map((e, idx) => {
+        return (
+          <ModalActionItem
+            key={idx}
+            openConfirmation={openConfirmation}
+            {...e}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+export default React.memo(ModalActionItemsContainer, (prev, next) => {
+  // don't change anything if the next's props has no actions
+  // console.log("next",next.actions);
+  // console.log("prev",prev.actions);
+  if (next.actions.length) return false;
+  return true;
+});
