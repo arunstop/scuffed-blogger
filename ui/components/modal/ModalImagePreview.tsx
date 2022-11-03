819f1175886665afdf54ae5b32dcc22f7910d618
arunstop <arunstop@gmail.com>
2022-11-03 22:10:39 +0700

    update : implemented new ModalImagePreview on /article thumbnail

---
+++ ui/components/modal/ModalImagePreview.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from "react";
+import { MdClose } from "react-icons/md";
+import { unblur } from "../../../app/helpers/UiHelpers";
+
+function ModalImagePreview({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <a
+      className={`group relative isolate  flex  cursor-pointer overflow-hidden 
+      transition-[width] duration-300 
+      focus-within:fixed  focus-within:inset-0 focus-within:z-[100] 
+      focus-within:aspect-auto focus-within:cursor-default focus-within:rounded-none  
+      focus-within:p-2 focus-within:backdrop-blur-sm sm:focus-within:p-4
+      ${className || ""}`}
+      tabIndex={0}
+    >
+      <div
+        className="absolute inset-0 group-focus-within:block hidden bg-black/60 animate-fadeIn"
+        onClick={unblur}
+      ></div>
+      <button
+        className="btn-primary btn-circle btn absolute right-0 top-0 z-[20] m-2 hidden 
+      group-focus-within:inline-flex sm:m-4"
+        onClick={() => {
+          (document.activeElement as HTMLElement).blur();
+        }}
+      >
+        <MdClose className="text-2xl text-white sm:text-3xl" />
+      </button>
+      {children}
+    </a>
+  );
+}
+
+export default React.memo(ModalImagePreview);
--- ui/layouts/article/pages/LayoutArticlePageSlug.tsx
+++ ui/layouts/article/pages/LayoutArticlePageSlug.tsx
@@ -2,34 +2,34 @@ import { formatDistance } from "date-fns";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 import {
-  MdClose,
   MdForum,
   MdMoreVert,
   MdRefresh,
   MdStar,
-  MdTrendingUp,
+  MdTrendingUp
 } from "react-icons/md";
+import { useAuthCtx } from "../../../../app/contexts/auth/AuthHook";
+import { autoRetry } from "../../../../app/helpers/MainHelpers";
 import {
   serviceArticleContentGet,
-  serviceArticleUpdateView,
+  serviceArticleUpdateView
 } from "../../../../app/services/ArticleService";
 import { ArticleModel } from "../../../../base/data/models/ArticleModel";
 import ArticleSectionAction from "../../../components/article/ArticleActions";
 import ArticleProgressBar from "../../../components/article/ArticleProgressBar";
 import Container from "../../../components/common/Container";
-import IntersectionObserverTrigger from "../../../components/utils/IntesectionObserverTrigger";
+import Dropdown, { DropdownOption } from "../../../components/common/Dropdown";
 import MainMarkdownContainer from "../../../components/main/MainMarkdownContainer";
 import MainPostStatusChip from "../../../components/main/MainPostFilterChip";
 import MainUserPopup from "../../../components/main/MainPostUserPopup";
 import MobileHeader, {
-  MobileHeaderActionProps,
+  MobileHeaderActionProps
 } from "../../../components/main/MobileHeader";
+import ModalImagePreview from "../../../components/modal/ModalImagePreview";
 import LoadingIndicator from "../../../components/placeholder/LoadingIndicator";
 import UserHeader from "../../../components/user/UserHeader";
+import IntersectionObserverTrigger from "../../../components/utils/IntesectionObserverTrigger";
 import LayoutArticleMoreSection from "../LayoutArticleMoreSection";
-import { autoRetry } from "../../../../app/helpers/MainHelpers";
-import { useAuthCtx } from "../../../../app/contexts/auth/AuthHook";
-import Dropdown, { DropdownOption } from "../../../components/common/Dropdown";
 
 function LayoutArticlePageSlug({
   articleContentless,
@@ -201,23 +201,7 @@ function LayoutArticlePageSlug({
           {article?.desc || `Article's Description`}
         </h2>
         <div className="flex flex-col gap-2 sm:gap-4">
-          <a
-            className="group relative isolate  flex
-            aspect-video w-full cursor-pointer overflow-hidden rounded-xl transition-all
-            duration-300 focus-within:fixed focus-within:inset-0 focus-within:z-[100]  focus-within:aspect-auto 
-            focus-within:cursor-default focus-within:rounded-none focus-within:bg-black/60 
-            focus-within:p-2 focus-within:backdrop-blur-sm sm:focus-within:p-4"
-            tabIndex={0}
-          >
-            <button
-              className="btn-outline btn-circle btn absolute right-0 top-0 z-[20] m-2 hidden border-white 
-              group-focus-within:inline-flex sm:m-4"
-              onClick={() => {
-                (document.activeElement as HTMLElement).blur();
-              }}
-            >
-              <MdClose className="text-2xl text-white sm:text-3xl"></MdClose>
-            </button>
+          <ModalImagePreview className="rounded-xl">
             <img
               className="z-10 h-full w-full max-w-none bg-primary object-cover transition-transform duration-500
               hover:scale-[1.2] group-focus-within:my-auto group-focus-within:h-auto group-focus-within:max-h-[90vh]
@@ -233,7 +217,7 @@ function LayoutArticlePageSlug({
               width={240}
               height={240}
             />
-          </a>
+          </ModalImagePreview>
         </div>
 
         {article.content ? (
