1100cea14ae50853208c64057e371b4f06ef7737
arunstop <arunstop@gmail.com>
2022-11-09 18:31:15 +0700

    update : removed route history debugging display

--- ui/layouts/main/LayoutMainWrapper.tsx
+++ ui/layouts/main/LayoutMainWrapper.tsx
@@ -1,30 +1,20 @@
 import { ReactNode } from "react";
-import { useAuthCtx } from "../../../app/contexts/auth/AuthHook";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import ScrollTopButton from "../../components/main/ScrollTopButton";
 import SearchModal from "../../components/main/SearchModal";
 import Sidebar from "../../components/main/Sidebar";
-import ToastContainer from "./ToastContainer";
 import BottomBar from "./BottomBar";
-import { useAtom } from "jotai";
-import { routeHistoryAtom } from "../../../app/hooks/RouteChangeHook";
-import { useRouter } from "next/router";
+import ToastContainer from "./ToastContainer";
 
 function LayoutMainWrapper({ children }: { children: ReactNode }) {
-  const {
-    isLoggedIn,
-    authStt: { user },
-  } = useAuthCtx();
-  const [history] = useAtom(routeHistoryAtom);
-  const router = useRouter();
   return (
     <>
-      <div className="fixed inset-0 bg-red-500/20 pointer-events-none z-[200]">
+      {/* <div className="fixed inset-0 bg-red-500/20 pointer-events-none z-[200]">
         {history[history.length - 1]}
         {<br/>}
         {router.asPath}
-      </div>
+      </div> */}
       {/* Header for desktop */}
       <Header />
       {/* Children */}
