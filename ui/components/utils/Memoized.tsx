c6a2d1af1f0f094e35124a4b2a5fdf318924495c
arunstop <arunstop@gmail.com>
2022-11-11 21:15:19 +0700

    update : added `once` props on `Memoized` to render only once

--- ui/components/utils/Memoized.tsx
+++ ui/components/utils/Memoized.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 
 interface MemoizedProps {
   show: boolean;
+  once?:boolean;
   children: ReactNode;
 }
 
@@ -11,7 +12,7 @@ function Memoized({ show, children }: MemoizedProps) {
 
 export default React.memo(Memoized, (prev, next) => {
   // if showing static show value then memoize it
-  if (prev.show === true && next.show === true) return true;
+  if (prev.once === true || next.once === true) return true;
   // or memoize when getting hidden while the previous state is showing
   const hiding = prev.show === true && next.show === false;
   return hiding === true;
