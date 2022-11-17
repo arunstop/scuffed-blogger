2dfa307d6e3a49590273358de844c5310aa673fe
scuffed-developer <scuffed.developer@gmail.com>
2022-11-17 19:21:11 +0700

    fix : next config not using the config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   output: "standalone",
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
@@ -15,4 +15,4 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV !== "production",
 });
 
-module.exports = withBundleAnalyzer(withPWA({}));
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
