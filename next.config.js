2dd513630a93c1bba980554ff5c7cbc1a8cd2e7b
arunstop <arunstop@gmail.com>
2022-11-17 09:13:18 +0700

    update : added pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-}
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withBundleAnalyzer(withPWA({}));
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 import "../styles/globals.css";
 // import { ArticleModel } from "../utils/data/models/ArticleModel";
 import dynamic from "next/dynamic";
-import  Head  from "next/head";
+import Head from "next/head";
 import { parseCookies } from "nookies";
 import SplashScreen from "../ui/components/placeholder/SplashScreen";
 import { isUserModel, UserModel } from "../base/data/models/UserModel";
@@ -53,6 +53,71 @@ const MainApp = ({
         <title>{`${APP_NAME} - Let your voice be heard`}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content={APP_DESC} />
+
+        {/* PWA */}
+
+        <meta name="application-name" content="Tuturku" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Tuturku" />
+        <meta name="description" content="Let your voices be heard!" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/touch-icon-ipad.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/touch-icon-iphone-retina.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/icons/touch-icon-ipad-retina.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Tuturku" />
+        <meta property="og:description" content="Let your voices be heard!" />
+        <meta property="og:site_name" content="Tuturku" />
+        <meta property="og:url" content="https://tuturku.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://tuturku.vercel.app/icons/384.png"
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </Head>
       <LazyLayoutAppWrapper user={user}>
         <Component {...pageProps} />
